14402eeaf4d5748e9165855d33b8f23fc3a3f504
Abhishek Tyagi <53171622+AbhishekTyagi404@users.noreply.github.com>
2023-06-18 17:24:59 +0530

    Update telegram-chat-parser.py

--- telegram-chat-parser.py
+++ telegram-chat-parser.py
@@ -16,64 +16,62 @@ from datetime import datetime
 
 
 columns = [
-           "msg_id",
-           "sender",
-           "sender_id",
-           "reply_to_msg_id",
-           "date",
-           "msg_type",
-           "msg_content",
-           "has_mention",
-           "has_email",
-           "has_phone",
-           "has_hashtag",
-           "is_bot_command",
-          ]
+    "msg_id",
+    "sender",
+    "sender_id",
+    "reply_to_msg_id",
+    "date",
+    "msg_type",
+    "msg_content",
+    "has_mention",
+    "has_email",
+    "has_phone",
+    "has_hashtag",
+    "is_bot_command",
+]
 
 file_types = [
-              "animation",
-              "video_file",
-              "video_message",
-              "voice_message",
-              "audio_file",
-             ]
+    "animation",
+    "video_file",
+    "video_message",
+    "voice_message",
+    "audio_file",
+]
 
 mention_types = [
-                 "mention",
-                 "mention_name",
-                ]
+    "mention",
+    "mention_name",
+]
 
 null_name_counter = 0
 
 def parse_telegram_to_csv(jdata):
-    
     if jdata.get("name") is None:
-        global null_name_counter 
+        global null_name_counter
         null_name_counter += 1
         chat_name = f"UnnamedChat-{null_name_counter}"
     else:
         chat_name = re.sub(r'[\W_]+', u'', jdata.get("name"), flags=re.UNICODE)
     output_filepath = f"{chat_name}.csv"
 
-    
-    with open(output_filepath, "w", encoding="utf-8") as output_file:
+    with open(output_filepath, "w", encoding="utf-8-sig", newline="") as output_file:
         writer = csv.DictWriter(output_file, columns, dialect="unix", quoting=csv.QUOTE_NONNUMERIC)
         writer.writeheader()
-        
+
         for message in jdata["messages"]:
             if message["type"] != "message":
                 continue
-            
+
             msg_id = message["id"]
             sender = message["from"]
             sender_id = message["from_id"]
-            reply_to_msg_id = message["reply_to_message_id"] if "reply_to_message_id" in message else -1
+            reply_to_msg_id = message.get("reply_to_message_id", -1)
             date = message["date"].replace("T", " ")
             dt = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-            
-            msg_content = message["text"]
+
+            msg_content = message.get("text", "")
             msg_type = "text"
-            
+
             if "media_type" in message:
                 msg_type = message["media_type"]
                 if message["media_type"] == "sticker":
@@ -86,7 +84,7 @@ def parse_telegram_to_csv(jdata):
             elif "file" in message:
                 msg_type = "file"
                 msg_content = message["file"]
-            
+
             if "photo" in message:
                 msg_type = "photo"
                 msg_content = message["photo"]
@@ -96,20 +94,20 @@ def parse_telegram_to_csv(jdata):
             elif "location_information" in message:
                 msg_type = "location"
                 loc = message["location_information"]
-                msg_content = str(loc["latitude"]) + "," + str(loc["longitude"])
-            
+                msg_content = f"{loc['latitude']},{loc['longitude']}"
+
             has_mention = 0
             has_email = 0
             has_phone = 0
             has_hashtag = 0
             is_bot_command = 0
-            
-            if type(msg_content) == list:
+
+            if isinstance(msg_content, list):
                 txt_content = ""
                 for part in msg_content:
-                    if type(part) == str:
+                    if isinstance(part, str):
                         txt_content += part
-                    elif type(part) == dict:
+                    elif isinstance(part, dict):
                         if part["type"] == "link":
                             msg_type = "link"
                         elif part["type"] in mention_types:
@@ -122,27 +120,27 @@ def parse_telegram_to_csv(jdata):
                             has_hashtag = 1
                         elif part["type"] == "bot_command":
                             is_bot_command = 1
-                        
+
                         txt_content += part["text"]
                 msg_content = txt_content
-            
+
             msg_content = msg_content.replace("\n", " ")
-            
+
             row = {
-                "msg_id"          : msg_id,
-                "sender"          : sender,
-                "sender_id"       : sender_id,
-                "reply_to_msg_id" : reply_to_msg_id,
-                "date"            : date,
-                "msg_type"        : msg_type,
-                "msg_content"     : msg_content,
-                "has_mention"     : has_mention,
-                "has_email"       : has_email,
-                "has_phone"       : has_phone,
-                "has_hashtag"     : has_hashtag,
-                "is_bot_command"  : is_bot_command,
+                "msg_id": msg_id,
+                "sender": sender,
+                "sender_id": sender_id,
+                "reply_to_msg_id": reply_to_msg_id,
+                "date": date,
+                "msg_type": msg_type,
+                "msg_content": msg_content,
+                "has_mention": has_mention,
+                "has_email": has_email,
+                "has_phone": has_phone,
+                "has_hashtag": has_hashtag,
+                "is_bot_command": is_bot_command,
             }
-            
+
             writer.writerow(row)
 
     print(chat_name, "OK!")
@@ -158,14 +156,13 @@ if __name__ == "__main__":
         sys.exit()
 
     backup_filepath = sys.argv[1]
-    
-    with open(backup_filepath, "r", encoding="utf-8") as input_file:
+
+    with open(backup_filepath, "r", encoding="utf-8-sig") as input_file:
         contents = input_file.read()
         jdata = json.loads(contents)
-        
+
         if "chats" not in jdata:
             parse_telegram_to_csv(jdata)
         else:
             for chat in jdata["chats"]["list"]:
                 parse_telegram_to_csv(chat)
-        
